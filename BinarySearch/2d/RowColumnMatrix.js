185338be06a1660400e4673f8f6b1a7bbceb8252
Tolulope Malomo <malomo.tolz@gmail.com>
2023-03-03 11:56:41 +0100

    rename file and update description

--- BinarySearch/2d/RowColumnMatrix.js
+++ BinarySearch/2d/RowColumnMatrix.js
@@ -3,9 +3,10 @@ const testCases = [
     input: {
       arr: [
         /*
-        Note that we're dealing with an array where
-        every row is sorted and every column is sorted.
-        But the array itself is not strictly sorted.
+        Note that we're dealing with a Row Column Matrix/Array:
+        This is an Array/Matrix where every row is sorted and 
+        every column is sorted. But the array itself is not
+        strictly sorted.
         */
         [10, 20, 30, 40],
         [15, 25, 35, 45],
@@ -17,6 +18,18 @@ const testCases = [
     output: [2, 2]
   }
 ]
+
+/*
+Time Complexity: O(N)
+The max no of steps we can take is
+N - 1 steps moving from the last column to the first column
+And N - 1 steps moving from the first row to the last row
+That's N + N = 2N = O(N) (Since we don't look at constants)
+
+Space Complexity: O(1)
+We're storing just r and c which don't grow
+in size relative to the the size of the input
+*/
 
 const BS = (arr, target) => {
   //Assuming this is a square matrix (ie. no of rows = no of columns)
